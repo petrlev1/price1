8c9ab16057d336abff8cf7aaa16776757d977f48
Petrlev <levendeevp@gmail.com>
2017-09-13 14:25:55 +0400

    Разбитие кода по функциям

--- index.php
+++ index.php
@@ -1,69 +1,8 @@
 ﻿<?php
 
 
-$fp = file_get_contents("analnie_stimulatori.csv");
-
-$line = explode( "\n",$fp );
-$sumLines = count($line);
-
-
-for ($i = 0; $i < $sumLines; $i++) {
-	
-$lineCell = explode( ";",$line[$i] );
-
-$line1 = $lineCell[0].";".$lineCell[1].";".$lineCell[2].";".$lineCell[3].";".$lineCell[4].";".$lineCell[5].";".$lineCell[6].";".$lineCell[7].";".$lineCell[8].";".$lineCell[9].";".$lineCell[10].";".$lineCell[11].";".$lineCell[12].";";
-
-
-
-//переименование разделов
-			print str_replace ( '00-00001840','Анальные стимуляторы -> Интимная гигиена',
-			str_replace ('00-00009228','Анальные стимуляторы',
-			str_replace ('00-00011099','Анальные стимуляторы -> Керамика',
-			str_replace ('00-00000125','Анальные стимуляторы -> С вибрацией',
-			str_replace ('00-00011061','Анальные стимуляторы -> Стеклянные',
-			str_replace ('00-00000022','Анальные стимуляторы -> Без вибро',
-			str_replace ('00-00000026','Анальные стимуляторы -> Стимуляторы простаты',
-			str_replace ('00-00000035','Анальные стимуляторы -> Анальные цепочки, шарики',
-			$line1 ))))))));
-
-
-print $lineCell[1]; print $lineCell[2];
-
-$lineCellImg = explode ( ",",$lineCell[13] );
-
-
-
-$sumImg = count($lineCellImg);
-
-
-
-// Основная картинка
-print ";"; //print trim($lineCellImg[0]);
-
-print str_replace ("\"","",trim($lineCellImg[0]));
-
-print "|";
-
-
-    for ( $x = 1; $x < $sumImg; $x++  ) {
-	
-	//trim - убирает пробелы и кавычки вначале и в конце строки
-	print str_replace ("\"","",trim($lineCellImg[$x])); 
-	
-	
-	//убираем последний символ
-	$aa = $sumImg - 1;
-	if ( $x != $aa ) { print "|"; }
-	
-    }
-	
-	print ";";
-	
-	
-	
-	
-	//формирование цены
-	$price = $lineCell[6];
+//формирование цены
+function priceNacenka($price, $procent) {
 	
 	if ( $price <= 100 ) { $procent = 100; }
 			 
@@ -82,16 +21,94 @@ print "|";
 		 elseif ( $price > 3000 and $price <= 4000 ) { $procent = 20; }
 		
 		 elseif ( $price > 4000 ) { $procent = 15; }
-		 
-	$priceNacenka = floor ($price+$price*$procent/100);
 	
-	print $priceNacenka;
+	$FuncPriceNacenka = floor ($price+$price*$procent/100);
 	
-	print "<br>";
+	return $FuncPriceNacenka;
+}
+
+
+//переименование разделов
+function renameCategories ($FuncLine) {
 	
+	$renameCat = str_replace ( '00-00001840','Анальные стимуляторы -> Интимная гигиена',
+			str_replace ('00-00009228','Анальные стимуляторы',
+			str_replace ('00-00011099','Анальные стимуляторы -> Керамика',
+			str_replace ('00-00000125','Анальные стимуляторы -> С вибрацией',
+			str_replace ('00-00011061','Анальные стимуляторы -> Стеклянные',
+			str_replace ('00-00000022','Анальные стимуляторы -> Без вибро',
+			str_replace ('00-00000026','Анальные стимуляторы -> Стимуляторы простаты',
+			str_replace ('00-00000035','Анальные стимуляторы -> Анальные цепочки, шарики',
+			$FuncLine
+			 ))))))));
+			
+			return $renameCat;
 	
 }
 
 	
+	
+	
+	
+	
+
+
+$fp = file_get_contents("test4.csv");
+
+$line = explode( "\n",$fp );
+$sumLines = count($line);
+
+
+for ($i = 0; $i < $sumLines; $i++) {
+	
+$lineCell = explode( ";",$line[$i] );
+
+$line1 = $lineCell[0].";".$lineCell[1].";".$lineCell[2].";".$lineCell[3].";".$lineCell[4].";".$lineCell[5].";".$lineCell[6].";".$lineCell[7].";".$lineCell[8].";".$lineCell[9].";".$lineCell[10].";".$lineCell[11].";".$lineCell[12].";";
+
+$price = $lineCell[6];
+
+$priceNacenka = priceNacenka($price,$procent);
+
+
+//переименование разделов
+$renameCategories = renameCategories ($line1);
+print $renameCategories;
+
+
+
+$lineCellImg = explode ( ",",$lineCell[13] );
+
+
+$sumImg = count($lineCellImg);
+
+
+
+
+// Основная картинка - ставим символ после картинки + убираем кавычки и пробелы
+print str_replace ("\"","",trim($lineCellImg[0]));
+print "|";
+
+
+
+    for ( $x = 1; $x < $sumImg; $x++  ) {
+		
+	//trim - убирает пробелы и кавычки вначале и в конце строки
+	print str_replace ("\"","",trim($lineCellImg[$x])) . ";" . $priceNacenka;; 
+	
+	
+	//убираем последний символ - точнее ставим | везде кроме последней картинки
+	$aa = $sumImg - 1;
+	if ( $x != $aa ) { print "|"; }
+	
+    }
+	
+	print "<br>";
+	
+	
+
+	
+}
+
+
 
 ?>
